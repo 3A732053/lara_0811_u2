31f351f80660ec0ba1e06a77975c0ed5cb1c0901
Lin Yu Lun <wuyahayaa01@gmail.com>
2020-10-22 16:19:57 +0800

    練習1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+//練習1
+
+/*
+Route::get('/',function ()
+{
+   return 'welcome';
+});
+*/
+
+/*
+Route::get('r1',function ()
+{
+    return redirect('r2');
+});
+
+Route::get('r2',function ()
+{
+    return view('welcome');
+});
+*/
+
+
+
+
+
+
+
+
+
